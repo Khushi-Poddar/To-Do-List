d8040b8466cffa7d897a0dbb82adc10608322837
Khushi <khushipoddar01071999@gmail.com>
2020-11-26 14:05:42 +0530

    updated project

--- app.js
+++ app.js
@@ -48,10 +48,6 @@ else
   res.render("list", {listTitle: "Today", newListItems: foundItems});
 }
 });
-
-
-
-
 });
 
 app.post("/", function(req, res){
@@ -73,9 +69,7 @@ const item = new Item({
           res.redirect("/" + listName);
         })
       }
-
-
-});
+    });
 
 const listSchema={
   name:String,
@@ -114,36 +108,35 @@ List.findOne({name:customListName},function(err,results){
 app.get("/about", function(req, res){
   res.render("about");
 });
+
 app.post("/delete",function(req,res){
  const checkeditemID=req.body.checkbox;
+const listName=req.body.listName;
 
- const listName = req.body.listName;
+if(listName==="Today")
+{
 
-if(listName ==="Today"){
-  Item.findByIdAndRemove( checkeditemID , function (err) {
-    if(err)
-    console.log(err);
-    else
-    {console.log("sucessful deletion");
- res.redirect("/");
- }
-  });
+   Item.findByIdAndRemove( checkeditemID , function (err) {
+    if(!err){
+  console.log("Sucessfuly deleted");
+res.redirect("/");
+    }
+
+});
 }
-else{
-  List.findOneAndUpdate({name: listName},{$pull :{items: {_id: checkeditemID}}} , function(err , foundList){
-    if(!err)
-    {
-      res.redirect("/" + listName);
+else
+{
+  List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkeditemID}}},function(err,foundList){
+    if(!err){
+      res.redirect("/"+listName);
     }
   });
 }
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
 
-app.listen(port, function() {
-  console.log("Server started ");
+});
+
+app.listen(process.env.port||3000, function() {
+  console.log("Server started on port 3000");
 });
